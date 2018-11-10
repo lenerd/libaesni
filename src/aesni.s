630be7a0fe013fcb20d55a221511c965e94add59
Lennart Braun <lenerd@posteo.de>
2018-11-10 21:53:57 +0100

    implement aesni_key_expansion_256

--- src/aesni.s
+++ src/aesni.s
@@ -59,9 +59,78 @@ movdqa 0xa0[rdi], xmm1
 ret
 
 
+.macro aes_key_expand_256_1 rcon
+# old in xmm1
+aeskeygenassist xmm4, xmm2, \rcon
+pshufd xmm4, xmm4, 0xff
+movdqa xmm3, xmm1
+pslldq xmm3, 4
+pxor xmm1, xmm3
+pslldq xmm3, 4
+pxor xmm1, xmm3
+pslldq xmm3, 4
+pxor xmm1, xmm3
+pxor xmm1, xmm4
+# new in xmm1
+.endm
+
+
+.macro aes_key_expand_256_2
+# old in xmm2
+aeskeygenassist xmm4, xmm1, 0
+pshufd xmm4, xmm4, 0xaa
+movdqa xmm3, xmm2
+pslldq xmm3, 4
+pxor xmm2, xmm3
+pslldq xmm3, 4
+pxor xmm2, xmm3
+pslldq xmm3, 4
+pxor xmm2, xmm3
+pxor xmm2, xmm4
+# new in xmm2
+.endm
+
+
 .globl aesni_key_expansion_256
 aesni_key_expansion_256:
 # rdi: pointer to output
 # rsi: pointer to key
-# TODO: implement
+movdqa xmm1, [rsi]
+movdqa [rdi], xmm1
+movdqa xmm2, 0x10[rsi]
+movdqa 0x10[rdi], xmm2
+
+aes_key_expand_256_1 0x01
+movdqa 0x20[rdi], xmm1
+aes_key_expand_256_2
+movdqa 0x30[rdi], xmm2
+
+aes_key_expand_256_1 0x02
+movdqa 0x40[rdi], xmm1
+aes_key_expand_256_2
+movdqa 0x50[rdi], xmm2
+
+aes_key_expand_256_1 0x04
+movdqa 0x60[rdi], xmm1
+aes_key_expand_256_2
+movdqa 0x70[rdi], xmm2
+
+aes_key_expand_256_1 0x08
+movdqa 0x80[rdi], xmm1
+aes_key_expand_256_2
+movdqa 0x90[rdi], xmm2
+
+aes_key_expand_256_1 0x10
+movdqa 0xa0[rdi], xmm1
+aes_key_expand_256_2
+movdqa 0xb0[rdi], xmm2
+
+aes_key_expand_256_1 0x20
+movdqa 0xc0[rdi], xmm1
+aes_key_expand_256_2
+movdqa 0xd0[rdi], xmm2
+
+aes_key_expand_256_1 0x40
+movdqa 0xe0[rdi], xmm1
+
 ret
