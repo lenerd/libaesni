f6bd03cf7f71f50b2e9020b25036704ecc066a38
Lennart Braun <lenerd@posteo.de>
2018-11-16 16:28:49 +0100

    add test and benchmark for multiple blocks

--- bench/bench_encrypt_block.cpp
+++ bench/bench_encrypt_block.cpp
@@ -54,3 +54,26 @@ static void BM_AES_256_ECB_single_blocks(benchmark::State& state)
             int64_t(state.range(0)));
 }
 BENCHMARK(BM_AES_256_ECB_single_blocks)->RangeMultiplier(4)->Range(16, 1 << 14);
+
+
+static void BM_AES_128_ECB(benchmark::State& state)
+{
+    const std::array<uint8_t, aesni_key_size_128> key = {
+        0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b,
+        0x0c, 0x0d, 0x0e, 0x0f
+    };
+    std::array<uint8_t, aesni_round_keys_size_128> round_keys = {};
+    aesni_key_expansion_128(round_keys.data(), key.data());
+
+    std::vector<uint8_t> input_blocks(state.range(0));
+    std::vector<uint8_t> output_blocks(state.range(0));
+    for (auto _ : state)
+    {
+        aesni_key_expansion_128(round_keys.data(), key.data());
+        aesni_encrypt_blocks_128(output_blocks.data(), input_blocks.data(),
+                round_keys.data(), input_blocks.size() / aesni_block_size);
+    }
+    state.SetBytesProcessed(int64_t(state.iterations()) *
+            int64_t(state.range(0)));
+}
+BENCHMARK(BM_AES_128_ECB)->RangeMultiplier(4)->Range(16, 1 << 14);
